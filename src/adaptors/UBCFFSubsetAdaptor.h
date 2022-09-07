4a22964363071bb93f0df7ed68d8b0d85236d0cb
thomas_lucky13 <mkoul@mail.ru>
2022-09-07 14:54:14 +0400

    vectorItem in Adaptors

--- src/adaptors/UBCFFSubsetAdaptor.h
+++ src/adaptors/UBCFFSubsetAdaptor.h
@@ -111,6 +111,7 @@ private:
         inline bool parseSvgPolygon(const QDomElement &element);
         inline bool parseSvgPolyline(const QDomElement &element);
         inline bool parseSvgLine(const QDomElement &element);
+        inline bool parseSvgVector(const QDomElement &element);
         inline bool parseSvgText(const QDomElement &element);
         inline bool parseSvgTextarea(const QDomElement &element);
         inline bool parseSvgImage(const QDomElement &element);
